ae5361f7eeb9a4a462456d41e156e543dc60f825
Thomas Meyer <accounts@protothomas.com>
2022-10-31 17:58:03 +0100

    feat: add basic extract command

--- extractor/cli.py
+++ extractor/cli.py
@@ -1,5 +1,6 @@
 """This module provides the extractor CLI."""
 
+from pathlib import Path
 from typing import Optional
 import typer
 from extractor import __app_name__, __version__
@@ -23,3 +24,24 @@ def main(
     )
 ) -> None:
     return
+
+@app.command()
+def extract(
+    xc_path: str = typer.Argument(...)
+) -> None:
+    """Extracts the build settings from a given Xcode installation."""
+    xcode = Path(xc_path)
+
+    if not xcode.suffix == ".app":
+        _showError(f'Path is not an application: {xcode.suffix}')
+    if not xcode.exists():
+         _showError('Xcode not found')
+    else:
+        plugin_path = xcode.joinpath('Contents', 'PlugIns')
+        for spec_file in plugin_path.glob('**/*.xcspec'):
+            typer.secho(f'Path: {spec_file}')
+        
+
+def _showError(txt: str):
+    typer.secho(txt, fg=typer.colors.RED)
+    raise typer.Exit(1)
