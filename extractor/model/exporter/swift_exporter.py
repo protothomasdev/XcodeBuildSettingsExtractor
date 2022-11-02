34a703ac43cc887b23b6403bacc18f5aa0b88164
Thomas Meyer <accounts@protothomas.com>
2022-11-02 23:19:32 +0100

    feat: write to swift file in exporter

--- extractor/model/exporter/swift_exporter.py
+++ extractor/model/exporter/swift_exporter.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import List
 from .export_interface import ExportInterface
+from ..formatter import to_string
 from ..setting import Setting
 
 class SwiftExporter(ExportInterface):
@@ -8,4 +9,5 @@ class SwiftExporter(ExportInterface):
     @classmethod
     def export(cls, settings: List[Setting], output: Path) -> Path:
         with output.open('w') as out:
-            pass
+            for s in settings:
+                out.write(to_string(s))
